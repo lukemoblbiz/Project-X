c27d7daae0ab8c1e0771457859d19d86f48163a4
lukemobley <139244336+lukemobley@users.noreply.github.com>
2024-01-31 11:07:47 -0600

    Updates

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { Client, Events, GatewayIntentBits, MessageEmbed } = require('discord.js');
+const { Client, Events, GatewayIntentBits, MessageEmbed, ApplicationCommandOptionType } = require('discord.js');
 const newCollection = require('./newCollection.js');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
@@ -18,10 +18,43 @@ client.on("ready", () => {
     console.log(`Logged is as ${client.user.tag}!`);
 });
 
+const commands = [
+    {
+        name: 'nftstats',
+        description: 'Returns stats of a Magic Eden collection',
+        options: [
+            {
+                name: 'symbol',
+                description: 'Enter the symbol of the collection',
+                type: ApplicationCommandOptionType.String,
+                required: true
+            }
+        ] 
+    },
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env['TOKEN']);
+
+(async () => {
+    try {
+        console.log('Registering slash commands...');
+
+        await rest.put(
+            Routes.applicationGuildCommands(process.env['CLIENT_ID'], process.env['GUILD_ID']),
+            {body: commands}
+        )
+
+        console.log('Slash commands were registered successfully');
+    } catch (error) {
+        console.log(`There was an error ${error}`)
+    }
+})();
+
 client.on('interactionCreate', (interaction) => {
     if (!interaction.isChatInputCommand()) return;
-        if (interaction.commandName === 'stats') {
-            
+        if (interaction.commandName === 'nftstats') {
+            const theSymbol = interaction.options.get('symbol');
+            return theSymbol;
         }
     }
 );
